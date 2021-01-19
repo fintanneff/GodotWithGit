c916611f366b6d8b115547717252ed58fe42a7a1
craigslisttrashman <fintanneff@gmail.com>
2021-01-18 21:37:29 -0500

    The bean now moves and jumps
    The bean now moves and jumps

--- PlatDemo/Player/PlatPlayer.gd
+++ PlatDemo/Player/PlatPlayer.gd
@@ -1,12 +1,25 @@
 extends KinematicBody2D
 
 onready var Spr = $Spr
-var yvel = 0
+var movevec = Vector2.ZERO
 
 func _physics_process(delta):
+	#Move
+	var inputx = Input.get_action_strength("ui_right")-Input.get_action_strength("ui_left")
+	if (abs(inputx) > 0.5):
+		movevec.x = inputx
+		movevec.x *= 100
+		Spr.global_scale = Vector2(sign(movevec.x), 1)
+	else:
+		movevec.x = 0
 	#Grav
 	if (is_on_floor()):
-		yvel = 2
+		movevec.y = 2
+		if (Input.is_action_just_pressed("ui_up")):
+			movevec.y = -200
 	else:
-		yvel += 1
-	move_and_slide(Vector2.DOWN*yvel, Vector2.UP, true)
+		movevec.y += 7
+		if (Input.is_action_just_released("ui_up") && movevec.y < 0):
+			movevec.y /= 3
+	#ApplyMove
+	move_and_slide(movevec, Vector2.UP, true)
