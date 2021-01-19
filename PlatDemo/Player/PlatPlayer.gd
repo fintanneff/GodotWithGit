7a9d7c5142eb171cf2fe069213a719d0a5b8c36f
craigslisttrashman <fintanneff@gmail.com>
2021-01-18 21:50:25 -0500

    oh dear
    oh dear

--- PlatDemo/Player/PlatPlayer.gd
+++ PlatDemo/Player/PlatPlayer.gd
@@ -3,7 +3,7 @@ extends KinematicBody2D
 onready var Spr = $Spr
 var movevec = Vector2.ZERO
 
-func _physics_process(delta):
+func _process(delta):
 	#Move
 	var inputx = Input.get_action_strength("ui_right")-Input.get_action_strength("ui_left")
 	if (abs(inputx) > 0.5):
@@ -12,6 +12,8 @@ func _physics_process(delta):
 		Spr.global_scale = Vector2(sign(movevec.x), 1)
 	else:
 		movevec.x = 0
+
+func _physics_process(delta):
 	#Grav
 	if (is_on_floor()):
 		movevec.y = 2
@@ -22,4 +24,5 @@ func _physics_process(delta):
 		if (Input.is_action_just_released("ui_up") && movevec.y < 0):
 			movevec.y /= 3
 	#ApplyMove
-	move_and_slide(movevec, Vector2.UP, true)
+	move_and_slide(movevec.x * Vector2.RIGHT, Vector2.UP, true)
+	move_and_slide(movevec.y * Vector2.DOWN, Vector2.UP, true)
